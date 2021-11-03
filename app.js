6c788a74c13357fa2b66aad161e6ae864a55bb06
Maged Maher <69519286+MagedMaher550@users.noreply.github.com>
2021-11-03 18:20:56 +0200

    third commit

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ mongoose.connect(process.env.MONGODB_URI, {
         useNewUrlParser: true
     })
     .then(result => {
-        app.listen(process.env.PORT, HOST_NAME);
+        app.listen(process.env.PORT, process.env.HOST_NAME);
     })
     .catch(err => {
         console.log(err);
